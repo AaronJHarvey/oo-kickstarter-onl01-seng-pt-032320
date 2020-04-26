ee13cf693ec0a7d69cc1416fdf3d6ae55686baca
Aaron J Harvey <harvey.aaronj@gmail.com>
2020-04-26 20:31:30 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -1,14 +1,16 @@
-class Project 
-  attr_reader :title 
-  attr_accessor :backers
-  
+
+
+class Project
+  attr_reader :title, :backers
+
   def initialize(title)
-    @title = title 
+    @title = title
     @backers = []
   end
-  
-  def add_backer(name)
-    @backers << name 
-    
+
+  def add_backer(backer)
+    backers << backer
+    backer.backed_projects << self
   end
+
 end
